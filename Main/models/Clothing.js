fb4064b93248bc0f89d0eb8efbb3f35b356fe828
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-05-29 15:17:39 -0400

    Added Pants' Table

--- Main/models/Clothing.js
+++ Main/models/Clothing.js
@@ -31,7 +31,6 @@ const ClothingType = sequelize.define('ClothingType', {
 
 
 const Shirt = sequelize.define('Shirt', {
-    // Model attributes are defined here
     id: {
         type: DataTypes.INTEGER,
         allowNull: false,
@@ -62,6 +61,39 @@ const Shirt = sequelize.define('Shirt', {
     underscored: true,
     modelName: 'shirt',
 });
+
+const Pants = sequelize.define('Pants', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    color: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    clothing_type_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'clothing_type',
+            key: 'id'
+        }
+    }
+}, {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'pants',
+});
+
 
 
 
