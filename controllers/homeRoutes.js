fe26451fdfae90519d0728e058d558e356ee0f24
batwood99 <atwood@mail.usf.edu>
2023-05-30 19:20:11 -0400

    restructure

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,29 +18,7 @@ router.get('/landing', withAuth, async (req, res) => {
   }
 });
 
-// Update last_worn route
-router.post('/update-last-worn', withAuth, async (req, res) => {
-  try {
-    // Extract the clothing item ID from the request body
-    const { clothingItemId } = req.body;
 
-    // Find the clothing item in the database
-    const clothingItem = await Clothing.findOne({ where: { id: clothingItemId, userId: req.session.user_id } });
-
-    if (clothingItem) {
-      // Update the last_worn date to today's date
-      clothingItem.last_worn = moment().format('YYYY-MM-DD');
-      await clothingItem.save();
-
-      res.sendStatus(200);
-    } else {
-      res.status(404).json({ error: 'Clothing item not found' });
-    }
-  } catch (err) {
-    console.error('Error updating last_worn:', err);
-    res.status(500).json({ error: 'Failed to update last_worn' });
-  }
-});
 
 // Wardrobe route
 router.get('/wardrobe', withAuth, async (req, res) => {
---
+++ controllers/wardrobeRoutes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const apiRoutes = require('./apiRoutes');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+
+// Render index page
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Wardrobe Wizard' });
+});
+
+// Update last_worn route
+router.post('/update-last-worn', withAuth, async (req, res) => {
+  try {
+    // Extract the clothing item ID from the request body
+    const { clothingItemId } = req.body;
+
+    // Find the clothing item in the database
+    const clothingItem = await Clothing.findOne({ where: { id: clothingItemId, userId: req.session.user_id } });
+
+    if (clothingItem) {
+      // Update the last_worn date to today's date
+      clothingItem.last_worn = moment().format('YYYY-MM-DD');
+      await clothingItem.save();
+
+      res.sendStatus(200);
+    } else {
+      res.status(404).json({ error: 'Clothing item not found' });
+    }
+  } catch (err) {
+    console.error('Error updating last_worn:', err);
+    res.status(500).json({ error: 'Failed to update last_worn' });
+  }
+});
+
+module.exports = router;
