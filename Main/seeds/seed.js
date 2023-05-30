041ca881a4e8f6fa518e27aa6e21292f527e2911
batwood99 <atwood@mail.usf.edu>
2023-05-30 10:15:38 -0400

    created seed.json file for seed.js and edited landing and wardrobe pages

--- Main/seeds/seed.js
+++ Main/seeds/seed.js
@@ -1,46 +1,134 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const seedData = require('./seed.json');
 
+const ClothingType = sequelize.define('ClothingType', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'clothing_type',
+});
 
-//This line seedDataBase is a function that will be called in the server.js file and will seed the database with the data from the seed.json file
+const Shirt = sequelize.define('Shirt', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  color: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  clothing_type_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'clothing_type',
+      key: 'id',
+    },
+  },
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'shirt',
+});
+
+const Pants = sequelize.define('Pants', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  color: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  clothing_type_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'clothing_type',
+      key: 'id',
+    },
+  },
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'pants',
+});
+
+const FootWear = sequelize.define('FootWear', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  color: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  clothing_type_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'clothing_type',
+      key: 'id',
+    },
+  },
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'foot_wear',
+});
+
 const seedDataBase = async () => {
-    await sequelize.sync({ force: true });
+  await sequelize.sync({ force: true });
 
-    await ClothingType.bulkCreate([
-        {name: 'Shirt'},
-        {name: 'Pants'},
-        {name: 'Shoes'},
-    ]);
+  await ClothingType.bulkCreate([
+    { name: 'Shirt' },
+    { name: 'Pants' },
+    { name: 'Shoes' },
+  ]);
 
-    await Shirt.bulkCreate([
-        {name: 'T-Shirt', color: 'Red', clothing_type_id: 1},
-        {name: 'T-Shirt', color: 'Blue', clothing_type_id: 1},
-        {name: 'T-Shirt', color: 'Green', clothing_type_id: 1},
-        {name: 'T-Shirt', color: 'Yellow', clothing_type_id: 1},
-        {name: 'T-Shirt', color: 'Orange', clothing_type_id: 1},
-    ]);
+  await Shirt.bulkCreate(seedData.shirts);
+  await Pants.bulkCreate(seedData.pants);
+  await FootWear.bulkCreate(seedData.footwear);
 
-    await Pants.bulkCreate([
-        {name: 'Jeans', color: 'Blue', clothing_type_id: 2},
-        {name: 'Jeans', color: 'Black', clothing_type_id: 2},
-        {name: 'Dress Pants', color: 'Grey', clothing_type_id: 2},
-        {name: 'Slacks', color: 'White', clothing_type_id: 2},
-        {name: 'Shorts', color: 'Blue', clothing_type_id: 2},
-    ]);
-
-    await FootWear.bulkCreate([
-        {name: 'Sneakers', color: 'White', clothing_type_id: 3},
-        {name: 'Slippers', color: 'Black', clothing_type_id: 3},
-        {name: 'Loafers', color: 'Red', clothing_type_id: 3},
-        {name: 'Boots', color: 'Brown', clothing_type_id: 3},
-        {name: 'Sandals', color: 'Blue', clothing_type_id: 3},
-    ]);
-
-    console.log('Database seeded!');
-    process.exit(0);
+  console.log('Database seeded!');
+  process.exit(0);
 };
+
 seedDataBase();
-test
-
-
-    
