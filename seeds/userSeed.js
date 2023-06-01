3c2063625dc4d6f5e5b8133f8fa06e8debda94f6
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-05-31 22:29:52 -0400

    Added user seed

---
+++ seeds/userSeed.js
@@ -0,0 +1,47 @@
+const { User } = require('../models');
+const { create } = require('../models/Clothing');
+
+const userData = [
+    {
+      "name": "John Doe",
+      "email": "johndoe@example.com",
+      "password": "password1"
+    },
+    {
+      "name": "Jane Smith",
+      "email": "janesmith@example.com",
+      "password": "password2"
+    },
+    {
+      "name": "Michael Johnson",
+      "email": "michaeljohnson@example.com",
+      "password": "password3"
+    },
+    {
+      "name": "Emily Davis",
+      "email": "emilydavis@example.com",
+      "password": "password4"
+    },
+    {
+      "name": "David Wilson",
+      "email": "davidwilson@example.com",
+      "password": "password5"
+    },
+    {
+      "name": "Emma Thompson",
+      "email": "emmathompson@example.com",
+      "password": "password6"
+    }
+  ];
+
+try {
+  const createdUsers = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  console.log('Users created', createdUsers);
+} catch (err) {
+  console.log('Error creating users', err);
+}
+
+module.exports = createUsers;
