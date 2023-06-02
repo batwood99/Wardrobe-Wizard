b551e8543a66acf41238a337344686d85da03dcd
batwood99 <atwood@mail.usf.edu>
2023-06-01 21:17:36 -0400

    created addclothing.js and edited wardrobe.handlebars and server.js to execute POST request

--- server.js
+++ server.js
@@ -40,7 +40,6 @@ app.get('/', (req, res) => {
   res.render('login', { title: 'Wardrobe Wizard' });
 });
 
-
 // Include wardrobeRoutes
 const wardrobeRoutes = require('./controllers/wardrobeRoutes');
 app.use('/wardrobe', wardrobeRoutes);
@@ -48,6 +47,23 @@ app.use('/wardrobe', wardrobeRoutes);
 // Include landingRoutes
 const landingRoutes = require('./controllers/landingRoutes');
 app.use('/landing', landingRoutes);
+
+// Add route to handle adding a clothing item
+app.post('/api/clothing', (req, res) => {
+  Clothing.create({
+    type: req.body.type,
+    color: req.body.color,
+    description: req.body.description,
+    user_id: req.session.user_id // Assuming you have the user ID stored in the session
+  })
+    .then((newClothing) => {
+      res.status(200).json(newClothing);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening on port', PORT));
