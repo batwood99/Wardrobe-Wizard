885ee6dc7fd524d821d96cba2c7663528ce9b03f
batwood99 <atwood@mail.usf.edu>
2023-06-04 16:49:04 -0400

    got log in to work

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,3 @@
-const { loginUser, signupUser } = require('../../utils/auth');
-
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
@@ -8,37 +6,20 @@ const loginFormHandler = async (event) => {
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
-    try {
-      // Call the loginUser function from auth.js
-      await loginUser(email, password);
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
 
+    if (response.ok) {
       // If successful, redirect the browser to the landing page
       document.location.replace('/landing');
-    } catch (error) {
-      alert('Failed to log in. Please check your credentials and try again.');
+    } else {
+      alert(response.statusText);
     }
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (name && email && password) {
-    try {
-      // Call the signupUser function from auth.js
-      await signupUser(name, email, password);
-
-      // If successful, redirect the browser to the landing page
-      document.location.replace('/landing');
-    } catch (error) {
-      alert('Failed to sign up. Please try again.');
-    }
-  }
-};
-
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('#login-btn').addEventListener('click', loginFormHandler);
